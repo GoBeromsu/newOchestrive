6f8025b96a7c56191c34be2b92463206ededf4a8
goberomsu <310o@naver.com>
2022-09-10 21:11:12 +0900

    Feat : Making Room

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import http, { Server } from "http";
 import express from "express";
 import kurento from "kurento-client";
 import SocketIO from "socket.io";
+import { pipeline } from "stream";
 
 // Constants
 const settings = {
@@ -16,6 +17,9 @@ const settings = {
 const app = express();
 const asUrl = url.parse(settings.WEBSOCKETURL);
 const port = asUrl.port;
+
+let kurentoClient = null;
+let iceCandidateQueues = null; //엔드포인트 생성 전에 icecandidate가 생성 될 수 있기 때ㅑ문이다.
 
 const server = app.listen(port, () => {
   console.log("Kurento Tutorial started");
@@ -33,10 +37,9 @@ io.on("connection", (socket) => {
         console.log("User registered " + socket.id);
         break;
       case "joinRoom":
-        joinRoom(socket, message.roomName, function () {});
+        getRoom(socket, message.roomName, () => {});
         break;
-      case "leaveRoom":
-        break;
+
       case "leaveRoom":
         break;
       case "onIceCandidate":
@@ -49,23 +52,105 @@ io.on("connection", (socket) => {
 
 app.use(express.static(path.join(__dirname, "static")));
 
-function joinRoom(socket, roomName, callback) {
-  if (socket.rooms.has(roomName)) {
-    // 유저가 방을 만들어야한다.
-    //
+function joinRoom(socket, username, roomname, callback) {
+  getRoom(socket, roomname, (err, myRoom) => {
+    if (err) {
+      return callback(err);
+    }
+
+    myRoom.pipeline.create("WebRtcEndpoint", (err, outgoingMedia) => {
+      if (err) {
+        callback(err);
+      }
+      let user = {
+        id: socket.id, //user connection id
+        name: username,
+        outgoingMedia: outgoingMedia, // our outgoing webrtc endpoint
+        incomingMedia: {}, //incoming webrtc endpoint
+      };
+
+      // 보낼 endpoint들을 확인한다
+      let iceCandidateQueue = iceCandidateQueues[user.id];
+      if (iceCandidateQueue) {
+        while (iceCandidateQueue.length) {
+          let ice = iceCandidateQueue.shift();
+          user.outgoingMedia.addIceCandidate(ice.candidate);
+        }
+      }
+
+      // candidate 생성
+      user.outgoingMedia.on("OnIceCandidate", (event) => {
+        let candidate = kurento.register.complexTypes.IceCandidates(
+          event.candidate
+        );
+        socket.emit("message", {
+          event: "candidate",
+          userid: user.id,
+          candidate: candidate,
+        });
+      });
+
+      // 방의 유저들에게 새로운 유저가 왔음을 알린다
+      socket.to(roomname).emit("message", {
+        event: "newParticipantArrived",
+        userid: user.id,
+        username: user.name,
+      });
+
+      let existingUsers = [];
+      for (let i in myRoom.participants) {
+        if (myRoom.participants[i].id != user.id) {
+          existingUsers.push({
+            id: myRoom.participants[i].id,
+            name: myRoom.participants[i].name,
+          });
+        }
+      }
+
+      socket.emit("message", {
+        event: "existingParticipants",
+        existingUsers: existingUsers,
+        userid: user.id,
+      });
+
+      myRoom.participants[user.id] = user;
+    });
+  });
+}
+
+function getRoom(socket, roomname, callback) {
+  let myRoom = io.sockets.adapter.rooms[roomname] || { length: 0 };
+  let numClients = myRoom.length;
+
+  if (numClients == 0) {
+    socket.join(roomname, () => {
+      myRoom = io.socket.adapter.rooms[roomname];
+      getKurentoClient((err, kurento) => {
+        kurento.create("MediaPipeline", (err, pipeline) => {
+          myRoom.pipeline = pipeline;
+          myRoom.participants = {};
+          callback(null, myRoom);
+        });
+      });
+    });
   } else {
-    // 유저가 방에 접속해야 한다.
+    socket.join(roomname);
+    callback(null, myRoom);
   }
 }
 
 function getKurentoClient(callback) {
-  return kurento(settings.KURENTOURL, function (error, kurentoClient) {
+  if (kurentoClient !== null) {
+    return callback(null);
+  }
+
+  kurento(settings.KURENTOURL, (error, _kurentoClient) => {
     if (error) {
-      var message =
+      const message =
         "Coult not find media server at address " + settings.KURENTOURL;
       return callback(message + ". Exiting with error " + error);
     }
-
+    kurentoClient = _kurentoClient;
     callback(null, kurentoClient);
   });
 }
